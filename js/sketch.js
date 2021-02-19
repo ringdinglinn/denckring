6e948508e1ea8a78c71239c6dd88ed6dbd626921
spitz linn - lspitz <linn.spitz@zhdk.ch>
2021-02-19 12:02:31 +0100

    nothing

--- js/sketch.js
+++ js/sketch.js
@@ -18,9 +18,16 @@ var pressedSpotY;
 var index = 99;
 
 var rotations = [0,0,0,0,0];
+var startRot;
+
+var img1;
+var img2;
+var img3;
+var img4;
 
 function preload(){
 		data = loadJSON('../denkring-data.json');
+		img1 = loadImage('../hand1.png');
 }
 
 function redraw(){
@@ -38,6 +45,7 @@ function setup(){
 
 	drawCircles();
 	drawWords();
+	drawLines();
 
 	mouseX1 = mouseX;
 	mouseX0 = mouseX;
@@ -49,15 +57,49 @@ function draw(){
 	if (mouseDown){
 		background(255);
 		var initV = createVector(pwinMouseY-height/2, pwinMouseX-width/2);
-		var newV = createVector(winMouseY-height/2, winMouseX-width/2)
+		var newV = createVector(winMouseY-height/2, winMouseX-width/2);
 		var v = createVector(0,1,0);
-		var initAngle = initV.angleBetween(v);
-		var newAngle = newV.angleBetween(v);
-		var diff = newAngle - initAngle
+		var initAngle = initV.angleBetween(v) + radians(180);
+		var newAngle = newV.angleBetween(v) + radians(180); 
+		console.log("initAngle = " + degrees(initAngle));
+		console.log("newAngle  = " + degrees(newAngle));
+		var diff = newV.angleBetween(initV);
+		console.log("diff = " + degrees(diff));
 		drawCircles();
-		diff *= 30;
+		drawLines();
+		diff = degrees(diff);
+		diff *= 1.2;
 		rotations[index] += diff;
-		// draw words but different
+		rotations[index] %= 360;
+
+		//snapping-------
+		// var increment = 360 / data.data[index].length;
+
+		// for (let i = 1; i < data.data[index].length; i++){
+		// 	if (rotations[index] > (i-1) * increment && rotations[index] <= i * increment){
+		// 		console.log("case 1");
+		// 		if (diff > 0){
+		// 			console.log("case 2");
+		// 			console.log(rotations[index]);
+		// 			console.log((i-1) * increment);
+		// 			console.log(i * increment);
+		// 			console.log("---------");
+		// 			if (rotations[index] > (i-1) * increment + 0.4 * increment){
+		// 				rotations[index] = i * increment;
+		// 			}
+		// 			if (rotations[index] < (i-1) * increment + 0.2 * increment && (i-1) * increment != startRot){
+		// 				rotations[index] = (i-1) * increment;
+		// 			}
+		// 		} else if (diff < 0){
+		// 			if (rotations[index] < (i) * increment - 0.3 * increment){
+		// 				rotations[index] = (i-1) * increment;
+		// 			}
+		// 		}
+		// 	}
+		// }
+
+
+		console.log(rotations[index]);
 		for (var i = 0; i < data.data.length; i++){
 			for (var j = 0; j < data.data[i].length; j++){
 				push();
@@ -73,6 +115,7 @@ function draw(){
 function windowResized(){
 	resizeCanvas(windowWidth, windowHeight);
 	drawCircles();
+	drawLines();
 	drawWords();
 }
 
@@ -96,14 +139,21 @@ function drawCircles(){
 	}
 }
 
+function drawImages(){
+	image(img1, width/2, height/2);
+}
+
+function drawLines(){
+	line(width/2, height/2, width/2 + 2000, height/2);
+}
+
 function redraw(){
 	drawCircles();
+	drawLines();
 	drawWords();
 }
 
 function mousePressed(){
-	pressedSpotY = mouseY;
-	pressedSpotX = mouseX;
 	index = 99;
 	for (var i = 4; i >= 0; i--){
 		if (dist(width/2, height/2, mouseX, mouseY) <= radii[i*2+1]/2){
@@ -111,7 +161,10 @@ function mousePressed(){
 		}
 	}
 	if (dist(width/2, height/2, mouseX, mouseY) < radii[0]/2) index = 99;
-	if (index != 99) 	mouseDown = true;
+	if (index != 99){
+		mouseDown = true;
+		startRot = rotations[index];
+	}
 }
 
 function mouseReleased(){
